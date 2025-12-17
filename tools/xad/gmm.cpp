4893e0bb24e749b5596185ae484cbf7233150d5a
mpilipov <misha7538@live.ru>
2025-12-17 18:38:59 +0100

    Add new tool - XAD implementation for gmm, XAD for hello was modified

---
+++ tools/xad/gmm.cpp
@@ -0,0 +1,97 @@
+#include "gradbench/evals/gmm.hpp"
+#include "gradbench/main.hpp"
+#include <XAD/XAD.hpp>
+#include <cmath>
+#include <vector>
+
+using namespace gmm;
+using namespace xad;
+using namespace std;
+class GmmXAD : public Function<Input, JacOutput> {
+public:
+  GmmXAD(Input& input) : Function(input) {}
+
+  void compute(JacOutput& output) {
+
+    typedef xad::adj<double>  mode;
+    typedef mode::tape_type   tape_type;
+    typedef mode::active_type AD;
+
+    tape_type tape;  // tape initialization
+
+    // active variables initialization
+    std::vector<AD> alpha_ad(_input.k);
+    std::vector<AD> mu_ad(_input.d * _input.k);
+    std::vector<AD> q_ad(_input.k * _input.d);
+    // size l: k * (d * (d - 1) / 2)
+    std::vector<AD> l_ad(_input.l.size());
+
+    // copying data from input to the active variables
+    for (size_t i = 0; i < alpha_ad.size(); ++i)
+      alpha_ad[i] = _input.alpha[i];
+    for (size_t i = 0; i < mu_ad.size(); ++i)
+      mu_ad[i] = _input.mu[i];
+    for (size_t i = 0; i < q_ad.size(); ++i)
+      q_ad[i] = _input.q[i];
+    for (size_t i = 0; i < l_ad.size(); ++i)
+      l_ad[i] = _input.l[i];
+
+    for (auto& v : alpha_ad)  // for every v in the vector alpha_ad
+      tape.registerInput(v);  // registering input on the tape
+    for (auto& v : mu_ad)
+      tape.registerInput(v);
+    for (auto& v : q_ad)
+      tape.registerInput(v);
+    for (auto& v : l_ad)
+      tape.registerInput(v);
+
+    // start of the recording
+    tape.newRecording();
+
+    AD error;  // target function we want to minimize
+    // gmm::objective receives data pointers
+    gmm::objective(
+        _input.d, _input.k, _input.n, alpha_ad.data(), mu_ad.data(),
+        q_ad.data(), l_ad.data(), _input.x.data(), _input.wishart,
+        &error);  // the computation result will be recorded into error
+
+    // Registering output on the tape and setting seed f'(...)=1
+    tape.registerOutput(error);
+    xad::derivative(error) = 1.0;
+
+    // adjoint mode computation - calculating derivatives by every of 4
+    // parameters (alpha, mu, q, l)
+    tape.computeAdjoints();
+
+    // preparation of the output variables
+    output.d = _input.d;
+    output.k = _input.k;
+    output.n = _input.n;
+    output.alpha.resize(_input.k);
+    output.mu.resize(_input.d * _input.k);
+    output.q.resize(_input.k * _input.d);
+    output.l.resize(l_ad.size());
+
+    // reading results from derivative() results to the output variables
+    for (size_t i = 0; i < output.alpha.size(); ++i)
+      output.alpha[i] = xad::derivative(alpha_ad[i]);
+    for (size_t i = 0; i < output.mu.size(); ++i)
+      output.mu[i] = xad::derivative(mu_ad[i]);
+    for (size_t i = 0; i < output.q.size(); ++i)
+      output.q[i] = xad::derivative(q_ad[i]);
+    for (size_t i = 0; i < output.l.size(); ++i)
+      output.l[i] = xad::derivative(l_ad[i]);
+  }
+};
+
+int main(int argc, char* argv[]) {
+  return generic_main(
+      argc, argv,
+      {{"objective",
+        function_main<gmm::Objective>},  // calculation of the error function
+
+       {"jacobian",
+        function_main<GmmXAD>}});  // calculates jacobian - matrix of first
+                                   // derivatives for every parameter alpha, mu,
+                                   // q, l (full gradient of the gmm model)
+}
--- tools/xad/hello.cpp
+++ tools/xad/hello.cpp
@@ -3,49 +3,73 @@
 
 // Connecting XAD
 #include <XAD/XAD.hpp>
-
-class Double : public Function<hello::Input, hello::DoubleOutput> {
+using namespace xad;
+using namespace hello;
+template <typename T>
+T double_x(T x) {
+  return x * 2.0;
+}
+class HelloSquareXAD : public Function<hello::Input, hello::DoubleOutput> {
 public:
-  Double(hello::Input& input) : Function(input) {}
+  HelloSquareXAD(hello::Input& input) : Function(input) {}
 
   void compute(hello::DoubleOutput& output) {
     // Defining types
     // xad::adj<double> is a mode adjoint for double type
-    typedef xad::adj<double> mode;
-    typedef mode::tape_type tape_type;
+    typedef xad::adj<double>  mode;
+    typedef mode::tape_type   tape_type;
     typedef mode::active_type AD;
 
     // Tape initialization
     tape_type tape;
 
     // x - an active input variable
-    AD x = _input;
+    AD x = _input;  //(_input - is saved input at the constructor of class
+                    // Function (main.hpp))
+
+    // tape input
+    tape.registerInput(x);
+    tape.newRecording();
+    AD y =
+        hello::square(x);  // the comupted function itself (also hello:square)
+
+    // output is function y=x^2 value
+    output = y.value();
+  }
+};
+
+class HelloDoubleXAD : public Function<hello::Input, hello::DoubleOutput> {
+public:
+  HelloDoubleXAD(hello::Input& input) : Function(input) {}
+
+  void compute(hello::DoubleOutput& output) {
+    // Defining types
+    // xad::adj<double> is a mode adjoint for double type
+    typedef xad::adj<double>  mode;
+    typedef mode::tape_type   tape_type;
+    typedef mode::active_type AD;
+
+    // Tape initialization
+    tape_type tape;
+
+    // x - an active input variable
+    AD x = _input;  //(_input - is saved input at the constructor of class
+                    // Function (main.hpp))
 
     // tape input
     tape.registerInput(x);
 
     tape.newRecording();
 
-    // computation of the function
-    AD y = hello::square(x);
+    AD y = double_x(x);  // the comupted function itself (also hello:square)
 
-    // tape output
-    tape.registerOutput(y);
-
-    // set f'(y) = 1
-    // В документации используется глобальная функция derivative(y) = 1.0;
-    xad::derivative(y) = 1.0;
-
-    // computing of the derivatives using backward mode
-    tape.computeAdjoints();
-
-    // derivative by x
-    output = xad::derivative(x);
+    // output is function y=2*x value
+    output = y.value();
   }
 };
 
 int main(int argc, char* argv[]) {
   return generic_main(argc, argv,
-                      {{"square", function_main<hello::Square>},
-                       {"double", function_main<Double>}});
+                      {{"square", function_main<HelloSquareXAD>},
+                       {"double", function_main<HelloDoubleXAD>}});
 }
